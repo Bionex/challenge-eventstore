a6d11c4655c19a0d59b0dd641d23ffe56a250578
Andrew <andrew_bionex@hotmail.com>
2021-06-26 21:10:26 -0300

    fix IllegalState Exception
    IllegalStateException can now use the standard method getMessage properly

---
+++ src/main/java/net/intelie/challenges/IllegalStateException.java
@@ -0,0 +1,10 @@
+package net.intelie.challenges;
+
+public class IllegalStateException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;	
+	
+	IllegalStateException(String msg){		
+		super(msg);
+	}
+}
